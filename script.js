405541baf7194b908b508b08f1e12d98a531d609
sumayiaakter0123456-lgtm <sumayiaakter0123456@gmail.com>
2026-02-26 22:01:13 +0600

    add javascript questions and solutions

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const rejectedFilterBtn = document.getElementById('rejected-filter-btn')
 
 const allCardSection = document.getElementById('allCards');
 const mainContainer = document.querySelector('main')
+const filterSection = document.getElementById('filtered-section')
 function caleculateCount() {
     total.innerText = allCardSection.children.length
     interviewCount.innerText = interviewList.length
@@ -25,9 +26,33 @@ function toggleStyle(id) {
     allFilterBtn.classList.add('bg-gray-100', 'text-gray-500')
     interviewFilterBtn.classList.add('bg-gray-100', 'text-gray-500')
     rejectedFilterBtn.classList.add('bg-gray-100', 'text-gray-500')
-    console.log(id);
+
     const selected = document.getElementById(id)
-    console.log(selected);
+
     selected.classList.remove('bg-gray-100', 'text-gray-500')
     selected.classList.add('bg-blue-500', 'text-white')
 }
+
+
+mainContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('interview-btn')) {
+        const parenNode = event.target.parentNode.parentNode;
+        const mobileName = parenNode.querySelector('.mobileName').innerText
+        const heroName = parenNode.querySelector('.heroName').innerText
+        const status = parenNode.querySelector('.status').innerText
+        const notes = parenNode.querySelector('.notes').innerText
+        const cardInfo = {
+            mobileName,
+            heroName,
+            status,
+            notes
+        }
+        const mobileExist = interviewList.find(item => item.mobileName == cardInfo.mobileName)
+        parenNode.querySelector('.status').innerText = 'Interview'
+        if (!mobileExist) {
+            interviewList.push(cardInfo)
+
+        }
+        renderInterview()
+    }
+})
